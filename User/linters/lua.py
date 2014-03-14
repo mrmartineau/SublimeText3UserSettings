4e95d5cfa749d97b0e4e457904b99adb6fd7d7d9
Zander Martineau <zander@martineau.tv>
2014-03-14 11:20:36 +0000

    fix dodgy merge

--- User/linters/lua.py
+++
@@ -1,9 +0,0 @@
-from lint import Linter
-
-class Lua(Linter):
-    language = 'lua'
-    cmd = ('luac', '-p')
-    regex = '^luac: [^:]+:(?P<line>\d+): (?P<error>.+?)(?P<near> near .+)?'
-
-    def run(self, cmd, code):
-        return self.tmpfile(cmd, code, suffix='.lua')
--- User/linters/ruby.py
+++
@@ -1,6 +0,0 @@
-from lint import Linter
-
-class Ruby(Linter):
-    language = 'ruby'
-    cmd = ('ruby', '-wc')
-    regex = r'^.+:(?P<line>\d+):\s+(?P<error>.+)'
